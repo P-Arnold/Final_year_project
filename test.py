0e01b81194f17f31ed7178c6448095c99581181b
parnold-TCD <parnold@tcd.ie>
2020-02-04 13:35:50 +0000

    somewhat refined

--- test.py
+++ test.py
@@ -8,8 +8,6 @@ driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "neofour")
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-#driver.session().read_transaction(check_package, "atom")
-#driver.session().write_transaction(add_package, "atom")
 def add_package(tx, name):
     tx.run("CREATE (p:Package {name: $name}) ", name=name)
 
@@ -26,36 +24,12 @@ def create_reverse_dependency(tx,nameA,nameB):
     tx.run("MATCH (a:Package),(b:Package) WHERE a.name = $nameA AND b.name = $nameB CREATE (a)-[r:ReverseDepends]->(b)",nameA=nameA,nameB=nameB)
 
 class primaryParser(HTMLParser):
-   #Initializing lists
-
-   # lsEndTags = list()
-   # lsStartEndTags = list()
-   # lsComments = list()
-   #HTML Parser Methods
    def handle_starttag(self, tag, attrs):
-       # if tag == 'meta':
-       #     if attrs[1][0] == 'content':
-       #         self.packageName = attrs[1][1]
-       #         if not driver.session().read_transaction(check_package, self.packageName):
-       #             driver.session().write_transaction(add_package, self.packageName)
        if tag == 'a':
            if attrs[0][0] == 'class':
                if attrs[0][1] == 'package-name':
                    self.foundPackage = True
                    self.curUrl = attrs[1][1]
-               # print(attrs[1][1])
-           # if attrs[0][0] == 'href':
-           #     check_string = attrs[0][1]
-           #     check_string = check_string[-7:]
-           #     if check_string == 'revdeps':
-           #         self.reversDep = True
-           #         newParser.revDepsUrl = attrs[0][1]
-                   #Really I need to call another html parser here...
-       # if tag =='div':
-       #     # if attrs[0][1] == 'dependencies':
-       #     #     self.dep = True
-       #     if attrs[0][1] == 'reverse-dependencies':
-       #         self.reversDep = True
    def handle_data(self,data):
        if self.foundPackage:
            lastLoc = data.rfind('-')
@@ -65,52 +39,12 @@ class primaryParser(HTMLParser):
                #Create Node in server if one by the same name does not exist
                print("Creating node ",self.packageName)
                driver.session().write_transaction(add_package, self.packageName)
-           # print("Creating new packagePageParser()")
-           # newPackageParser = packagePageParser()
-           # newPackageParser.packageName = self.packageName
-           # newPackageParser.reverseDep = False
-           # packageRevDepsNum = 0
-           # newPackageParser.revDepsNum = packageRevDepsNum
-
            self.mainPageUrls.append(self.curUrl)
-
-           # PackageMainPage = urllib3.urlopen(self.curUrl)
-           # print("Opening: ", self.curUrl)
-           # newPackageParser.feed(str(PackageMainPage.read()))
-
-
-           #Go to PackagePageParser()
-
-           #Probably don't need this stuff anymore
-           # newParser = primaryParser()
-           # new_html_page = urllib3.urlopen(self.curUrl)
-           # newParser.feed(str(new_html_page.read()))
-           # newParser.packageName = ''
-           # # newParser.foundPackage = False
-           # # newParser.packageDetails = list()
-           # # newParser.packCount = 0
-           # # newParser.curUrl = ''
-           # # newParser.reversDep = False
-           # # newParser.dep = False
-           # newParser.revDepsUrl = ''
-
-           # print("Encountered some data  :", data)
-           # self.packageDetails.append((data,self.curUrl))
-           # self.packCount += 1
-           # print(self.packCount, data)
            self.foundPackage = False
-       # if self.dep:
-       #     print(data[:21], "\n")
-       #     self.dep = False
-       # if self.reversDep:
-       #     print(data[:18], "\n")
-       #     self.reversDep = False
-
 
 class packagePageParser(HTMLParser):
    def handle_starttag(self, tag, attrs):
        if tag == 'div':
-           # print(attrs)
            if attrs:
                if attrs[0][1] == 'reverse-dependencies':
                    self.reverseDep = True
@@ -123,18 +57,8 @@ class packagePageParser(HTMLParser):
                    if check_string == 'revdeps':
                        self.revDepsUrl = attrs[0][1]
                        print("revDepsUrl: ",self.revDepsUrl)
-                       #Enter full list link here
-                       # reverseDepPageParser = revDepPageParser()
-                       # reverseDepPageParser.mainPackageName = self.packageName
-                       # reverseDepPageParser.foundPackName = False
-
-                       # fullListPage = urllib3.urlopen(self.revDepsUrl)
-                       # reverseDepPageParser.feed(str(fullListPage.read()))
-
                        #could also maybe add something here to stop these parsers generally once finished with the full reverse dependency list
-
                        #maybe untruth the reversDep at end
-
    def handle_data(self,data):
        if self.reverseDep:
            if len(data) > 12:
@@ -144,10 +68,7 @@ class packagePageParser(HTMLParser):
                #I planned to add a cypher call here to check the number of stored reverse dependencies but I'm just gonna set it anyway
                # print("Setting reverse dependencie in ",self.packageName, " as: ",self.revDepsNum)
                # driver.session().write_transaction(set_num_rev_deps, self.packageName, int(self.revDepsNum)) #set the number of reverse dependencies found in html text
-
-
        #comments content
-
 
 class revDepPageParser(HTMLParser):
    def handle_starttag(self, tag, attrs):
@@ -155,7 +76,6 @@ class revDepPageParser(HTMLParser):
            if len(attrs)>1:
                if attrs[1][0] == 'title':
                    infoString = attrs[1][1]
-                   # mainName = self.mainPackageName
                    if infoString.find(self.mainPackageName):
                        self.foundPackName = True
    def handle_data(self,data):
@@ -174,7 +94,6 @@ class revDepPageParser(HTMLParser):
                driver.session().write_transaction(create_reverse_dependency,self.mainPackageName,packageToAddName)
            self.foundPackName = False
 
-       #comments content
 # creating an object of the overridden class
 curUrl = ''
 packCount = 0
@@ -200,17 +119,7 @@ html_page = urllib3.urlopen("https://www.stackage.org/lts-14.21")
 
 #Feeding the content
 parser.feed(str(html_page.read()))
-# for pack in parser.packageDetails:
-#     print("Package Name: ",pack[0])
-#     print("URL: ", pack[1])
-#     html_page = urllib3.urlopen(pack[1])
-#     parser.feed(str(html_page.read()))
 
-# urlLog = open("mainPageUrls.txt","a")
-# urlLog.write("\n")
-# urlLog.write(str(datetime.now()))
-# urlLog.close()
-# print(parser.mainPageUrls)
 for url in parser.mainPageUrls:
     print("Handling url:", url)
     html_page = urllib3.urlopen(url)
@@ -235,10 +144,3 @@ for url in parser.mainPageUrls:
         reverseDepPageParser.mainPackageName = newPackageParser.packageName
         reverseDepPageParser.foundPackName = False
         reverseDepPageParser.feed(str(fullListUrl.read()))
-
-#lets do the mainpage ur stuff here...
-# print(len(parser.mainPageUrls))
-#printing the extracted values
-# print(parser.lsStartTags)
-#print(“End tags”, parser.lsEndTags)
-#print(“Start End tags”, parser.lsStartEndTags)
